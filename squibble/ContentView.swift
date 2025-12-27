89b6cb3ca83c0bf836c2115a03cfedd94730ea9b
Mehul Pandey <mehul@stage9.ai>
2025-12-26 21:28:51 -0800

    Initial Commit

---
+++ squibble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  squibble
+//
+//  Created by Mehul Pandey on 12/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ squibble/squibbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  squibbleApp.swift
+//  squibble
+//
+//  Created by Mehul Pandey on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct squibbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
